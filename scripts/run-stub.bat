866c4221a2b87f790fc45b40130738a9cf3af283
r1ch11 <rawire@gmail.com>
2019-07-21 21:10:29 -0400

    Added comments to run-stub.bat

--- scripts/run-stub.bat
+++ scripts/run-stub.bat
@@ -1,15 +1,22 @@
 @echo off
-
+REM ----------------------------------------------------------------------
+REM --                                                                  --
+REM -- Use to run a manual test using the barcode scanner stub.         --
+REM --                                                                  --
+REM -- Assumption: Java is in the local file system and %JAVA_HOME%\bin --
+REM --             is set in the %PATH%.                                --
+REM --                                                                  --
+REM -- Usage:                                                           --
+REM --  (1) Open an empty text file named "test.txt" in Notepad and     --
+REM --      make sure it is not minimized.                              --
+REM --  (2) Run this script.                                            --
+REM --  (3) Observe that the barcode (defined in this script) is        --
+REM --      written to the "test.txt" file.                             --
+REM --                                                                  --
+REM ----------------------------------------------------------------------
 java -version
-
 set SCRIPT_DIR=%~dp0
-
-rem start javaw -jar %SCRIPT_DIR%Stub.jar
-
 set WINDOW_TITLE="test.txt - Notepad"
-
 set BARCODE="01-87625-0"
-
 java -jar %SCRIPT_DIR%Stub.jar %WINDOW_TITLE% %BARCODE%
-
 pause
